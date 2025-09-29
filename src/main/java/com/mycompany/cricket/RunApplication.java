be440e384f66dfedbeae55ceb5a1e8dc78c3440c
reggykoketso <reggykoketso@gmail.com>
2025-09-29 13:39:14 +0000

    changes

--- src/main/java/com/mycompany/cricket/RunApplication.java
+++ src/main/java/com/mycompany/cricket/RunApplication.java
@@ -3,21 +3,20 @@ package com.mycompany.cricket;
 import java.util.Scanner;
 
 public class RunApplication {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("The cricketer name: ");
-        String batsman = sc.nextLine();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the cricketer name: ");
+        String batsmanName = input.nextLine();
 
         System.out.print("Enter the stadium: ");
-        String stadium = sc.nextLine();
+        String stadiumName = input.nextLine();
 
-        System.out.print("Enter the total runs scored by " + batsman + " at " + stadium + ": ");
-        int runs = sc.nextInt();
+        System.out.print("Enter the total runs scored by " + batsmanName + " at " + stadiumName + ": ");
+        int runsScored = input.nextInt();
 
-        CricketRunsScored cr = new CricketRunsScored(batsman, stadium, runs);
+        CricketRunsScored cr = new CricketRunsScored(batsmanName, stadiumName, runsScored);
         cr.printReport();
-
-        sc.close();
     }
 }
